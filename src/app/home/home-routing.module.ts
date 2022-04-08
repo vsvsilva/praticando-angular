4ccc5af33d3abd5dc3275e645b863845cf64a640
Vitor <vitor_ovitor@outlook.com>
2022-04-08 15:57:26 -0300

    cabecalho

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,23 @@
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: HeaderComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { HomeRoutingModule } from './home-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
